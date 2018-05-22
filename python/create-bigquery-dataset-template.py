b0113a6917565200bf07746fc3b837c47f7630a8
Robert Sahlin <robert.sahlin@mathem.se>
2018-05-22 14:42:19 +0200

    added template to create bigquery dataset

---
+++ python/create-bigquery-dataset-template.py
@@ -0,0 +1,26 @@
+"""Creates pubsub topics and subscriptions for a new google analytics property."""
+# gcloud deployment-manager deployments create aws-kinesis-order-stream --template add-streaming-source.py --properties streamId:order-stream --preview
+# gcloud deployment-manager deployments update aws-kinesis-order-stream --template add-streaming-source.py --properties streamId:order-stream --preview
+# gcloud deployment-manager deployments create ga-property-ua-1234567-89 --template add-streaming-source.py --properties streamId:UA-1234567-89 --preview
+# gcloud deployment-manager deployments update ga-property-ua-1234567-89 --template add-streaming-source.py --properties streamId:UA-1234567-89 --preview
+
+def AlphaNum(stream):
+  return "".join([ c if c.isalnum() else "" for c in stream ])
+
+def GenerateConfig(context):
+  """Generate configuration."""
+
+  resources = []
+
+# BigQuery dataset to store entities from processor pipeline
+  resources.append({
+      'name': 'bigquery-dataset-' + context.properties['streamId'] + '-entities', #bigquery-dataset-order-stream-entities
+      'type': 'bigquery.v2.dataset',
+      'properties': {
+          'datasetReference': {
+              'datasetId': AlphaNum(context.properties['streamId'])
+          }
+      }
+  })
+
+  return {'resources': resources}
