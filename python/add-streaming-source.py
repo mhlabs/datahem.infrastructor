83fe886111667ac7b77beefd12bee62072bcf911
Robert Sahlin <robert.sahlin@mathem.se>
2018-06-07 14:53:27 +0200

    added bigquery ua views template

--- python/add-streaming-source.py
+++ python/add-streaming-source.py
@@ -1,11 +1,9 @@
 """Creates pubsub topics and subscriptions for a new google analytics property."""
-# gcloud deployment-manager deployments create aws-kinesis-order-stream --template add-streaming-source.py --properties streamId:order-stream,location:EU
-# gcloud deployment-manager deployments update aws-kinesis-order-stream --template add-streaming-source.py --properties streamId:order-stream,location:EU
-# gcloud deployment-manager deployments create ga-property-ua-1234567-89 --template add-streaming-source.py --properties streamId:UA-1234567-89,location:EU
-# gcloud deployment-manager deployments update ga-property-ua-1234567-89 --template add-streaming-source.py --properties streamId:UA-1234567-89,location:EU
+# gcloud deployment-manager deployments create aws-kinesis-orderstream --template add-streaming-source.py --properties streamId:orderstream,location:EU
+# gcloud deployment-manager deployments create ga-property-ua123456789 --template add-streaming-source.py --properties streamId:ua123456789,location:EU
 
 def AlphaNum(stream):
-  return "".join([ c if c.isalnum() else "" for c in stream ])
+  return "".join([ c if c.isalnum() else "" for c in stream ]).lower()
 
 def GenerateConfig(context):
   """Generate configuration."""
@@ -14,45 +12,45 @@ def GenerateConfig(context):
 
 # topic used by collector to push measurement protocol payloads
   resources.append({
-      'name': 'pubsub-topic-' + context.properties['streamId'], #pubsub-topic-order-stream
+      'name': 'pubsub-topic-' + AlphaNum(context.properties['streamId']), #pubsub-topic-ua123456789
       'type': 'pubsub.v1.topic',
       'properties': {
-          'topic': context.properties['streamId'] #order-stream
+          'topic': AlphaNum(context.properties['streamId']) #ua123456789
       }
   })
 
 # subscription used to backup payloads to bigquery
   resources.append({
-      'name': 'pubsub-subscription-' + context.properties['streamId'] + '-backup', #pubsub-subscription-order-stream-backup
+      'name': 'pubsub-subscription-' + AlphaNum(context.properties['streamId']) + '-backup', #pubsub-subscription-ua123456789-backup
       'type': 'pubsub.v1.subscription',
       'properties': {
-          'subscription': context.properties['streamId'] + '-backup', #order-stream-backup
-          'topic': '$(ref.pubsub-topic-' + context.properties['streamId'] + '.name)' #order-stream
+          'subscription': AlphaNum(context.properties['streamId']) + '-backup', #ua123456789-backup
+          'topic': '$(ref.pubsub-topic-' + AlphaNum(context.properties['streamId']) + '.name)' #ua123456789
       }
   })
 
 # subscription used for processor pipeline that transforms measurement protocol payloads to events
   resources.append({
-      'name': 'pubsub-subscription-' + context.properties['streamId'] + '-processor', #pubsub-subscription-order-stream-procssor
+      'name': 'pubsub-subscription-' + AlphaNum(context.properties['streamId']) + '-processor', #pubsub-subscription-ua123456789-procssor
       'type': 'pubsub.v1.subscription',
       'properties': {
-          'subscription': context.properties['streamId'] + '-processor', #order-stream-processor
-          'topic': '$(ref.pubsub-topic-' + context.properties['streamId'] + '.name)' #order-stream
+          'subscription': AlphaNum(context.properties['streamId']) + '-processor', #ua123456789-processor
+          'topic': '$(ref.pubsub-topic-' + AlphaNum(context.properties['streamId']) + '.name)' #ua123456789
       }
   })
 
 # topic used by processor pipeline to push events as stream
   resources.append({
-      'name': 'pubsub-topic-' + context.properties['streamId'] + '-entities', #pubsub-topic-order-stream-entities
+      'name': 'pubsub-topic-' + AlphaNum(context.properties['streamId']) + '-entities', #pubsub-topic-ua123456789-entities
       'type': 'pubsub.v1.topic',
       'properties': {
-          'topic': context.properties['streamId']+ '-entities' #order-stream-entities
+          'topic': AlphaNum(context.properties['streamId'])+ '-entities' #ua123456789-entities
       }
   })
 
 # BigQuery dataset to store entities from processor pipeline
   resources.append({
-      'name': 'bigquery-dataset-' + context.properties['streamId'] + '-entities', #bigquery-dataset-order-stream-entities
+      'name': 'bigquery-dataset-' + AlphaNum(context.properties['streamId']) + '-entities', #bigquery-dataset-ua123456789-entities
       'type': 'bigquery.v2.dataset',
       'properties': {
           'datasetReference': {
--- python/create-bigquery-dataset-template.py
+++ python/create-bigquery-dataset-template.py
@@ -1,8 +1,5 @@
-"""Creates pubsub topics and subscriptions for a new google analytics property."""
-# gcloud deployment-manager deployments create aws-kinesis-order-stream --template add-streaming-source.py --properties streamId:order-stream,location:EU
-# gcloud deployment-manager deployments update aws-kinesis-order-stream --template add-streaming-source.py --properties streamId:order-stream,location:EU
-# gcloud deployment-manager deployments create ga-property-ua-1234567-89 --template add-streaming-source.py --properties streamId:UA-1234567-89,location:EU
-# gcloud deployment-manager deployments update ga-property-ua-1234567-89 --template add-streaming-source.py --properties streamId:UA-1234567-89,location:EU
+"""Creates a bigquery dataset."""
+# gcloud deployment-manager deployments create bigquery-backup-dataset --template create-bigquery-dataset.py --properties streamId:backup,location:EU
 
 def AlphaNum(stream):
   return "".join([ c if c.isalnum() else "" for c in stream ])
@@ -14,7 +11,7 @@ def GenerateConfig(context):
 
 # BigQuery dataset to store entities from processor pipeline
   resources.append({
-      'name': 'bigquery-dataset-' + context.properties['streamId'] + '-entities', #bigquery-dataset-order-stream-entities
+      'name': 'bigquery-dataset-' + AlphaNum(context.properties['streamId']) + '-entities', #bigquery-dataset-ua123456789-entities
       'type': 'bigquery.v2.dataset',
       'properties': {
           'datasetReference': {
